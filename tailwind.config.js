dbcb76e1ea98d8b68bc4faaf02d46fb14d56d43a
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-09-16 18:00:10 +0200

    Install components from heroUI and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import { heroui } from "@heroui/theme";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/@heroui/theme/dist/components/navbar.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
