315c6026842c4800569ee962ff392b7e988db98b
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-10-20 15:13:48 +0200

    Add theme provider

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { HeroUIProvider } from "@heroui/react";
+import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <HeroUIProvider>{children}</HeroUIProvider>
+    </ThemeProvider>
+  );
 }
