66dbe2f4b4a7028a313fb01e8e0cca24ae621503
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-11-17 19:54:37 +0100

    Add protected routes with middleware

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const token = await getToken({
+    req: request,
+    secret: process.env.AUTH_SECRET,
+  });
+  const protectedRoutes = ["/ingredients"];
+
+  const isProtectedRoute = () => {
+    return protectedRoutes.some((route) => pathname.startsWith(route));
+  };
+
+  if (isProtectedRoute()) {
+    if (!token) {
+      const url = new URL("/error", request.url);
+      url.searchParams.set("message", "Недостаточно прав");
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/ingredients"],
+};
